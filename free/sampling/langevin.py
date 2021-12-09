19f0ae4dee143dfa97c8d3d8cb602e0988d24eba
Балаганский Никита Николаевич <n.n.balaganskiy@tinkoff.ru>
2021-12-09 19:55:39 +0300

    fix jit

--- free/sampling/langevin.py
+++ free/sampling/langevin.py
@@ -3,20 +3,32 @@ import jax
 from free.sampling.base import Sampler
 
 
+
+
+
+
+
 class LangevinSampler(Sampler):
 
     step_size: float = 0.02
 
     def step(self, img, params, key, *args, **kwargs):
+
         @jax.jit
         def energy(img, params):
             return self.apply_fn(params, img).mean()
 
-        energy_grad = jax.grad(energy)
-        noise = jax.random.normal(key=key, shape=img.shape) * self.step_size * 2
-        e_grad = energy_grad(img, params)
-        e_grad = jax.lax.clamp(-0.01, e_grad, 0.01)
-        next_img = img - self.step_size * energy_grad(img, params) + noise
-        next_img = jax.lax.clamp(0., next_img, 1.)
-        return next_img
+        @jax.jit
+        def _step_impl(img, params, key):
+            energy_grad = jax.grad(energy)
+            noise = jax.random.normal(key=key, shape=img.shape) * self.step_size * 2
+            e_grad = energy_grad(img, params)
+            e_grad = jax.lax.clamp(-0.01, e_grad, 0.01)
+            next_img = img - self.step_size * e_grad + noise
+            next_img = jax.lax.clamp(0., next_img, 1.)
+            return next_img
+
+        return _step_impl(
+            img=img, params=params, key=key
+        )
 
